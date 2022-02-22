934534340b235dafea5881bccf92792ea1dc4568
Wenxuan Zhang <wenxuangm@gmail.com>
2022-02-22 15:17:55 +0800

    feat: improve benchmark scripts

--- scripts/benchmark_local.sh
+++ scripts/benchmark_local.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
 
+info() { printf "$(date -Is) %b[info]%b %s\n" '\e[0;32m\033[1m' '\e[0m' "$*" >&2; }
+
 usage() { echo "Usage: $(basename "$0") <feature_count>" >&2; }
 
 [ $# -ne 1 ] && usage && exit 1
@@ -15,6 +17,8 @@ ROWS=${ROWS:-50000}
 results_dir="$SDIR/results"
 
 prepare() {
+    info "prepare data..."
+
     ./gen_oomstore_meta.sh "$feature_count"
     ./gen_oomstore_data.sh "$feature_count"
 
@@ -26,20 +30,53 @@ prepare() {
 }
 
 bench_golang() {
-    id=$1
+    info "benchmark golang..."
+    concurrency=$1
     mkdir -p "$results_dir/golang"
     (
         cd ../golang
-        for concurrency in 1 2 4 8 16 32 64 128; do
-            go run ./online_get/main.go "$oomstore_cfg" "$REQUESTS" $concurrency "$ROWS" "$feature_count" \
-                > "$results_dir/golang/n${REQUESTS}_c${concurrency}_r${ROWS}_f${feature_count}.$id"
-        done
+        info "warm up..."
+        go run ./online_get/main.go "$oomstore_cfg" "$REQUESTS" "$concurrency" "$ROWS" "$feature_count"
+        info "benchmarking..."
+        go run ./online_get/main.go "$oomstore_cfg" "$REQUESTS" "$concurrency" "$ROWS" "$feature_count" \
+            >"$results_dir/golang/n${REQUESTS}_r${ROWS}_f${feature_count}_c${concurrency}"
+    )
+}
+
+bench_rust() {
+    info "benchmark rust..."
+    concurrency=$1
+    mkdir -p "$results_dir/rust"
+    (
+        cd ../rust
+        info "warm up..."
+        cargo run --release -- "$oomstore_cfg" "$REQUESTS" "$concurrency" "$ROWS" "$feature_count"
+        info "benchmarking..."
+        cargo run --release -- "$oomstore_cfg" "$REQUESTS" "$concurrency" "$ROWS" "$feature_count" \
+            >"$results_dir/rust/n${REQUESTS}_r${ROWS}_f${feature_count}_c${concurrency}"
+    )
+}
+
+bench_python() {
+    info "benchmark python..."
+    concurrency=$1
+    mkdir -p "$results_dir/python"
+    (
+        cd ../python
+        info "warm up..."
+        python online_get.py "$oomstore_cfg" "$REQUESTS" "$concurrency" "$ROWS" "$feature_count"
+        info "benchmarking..."
+        python online_get.py "$oomstore_cfg" "$REQUESTS" "$concurrency" "$ROWS" "$feature_count" \
+            >"$results_dir/python/n${REQUESTS}_r${ROWS}_f${feature_count}_c${concurrency}"
     )
 }
 
 bench_all() {
-    for id in {1..5}; do
-        bench_golang "$id"
+    for concurrency in 1 2 4 8 16 32 64 128; do
+        info "benchmark (concurrency=${concurrency})..."
+        bench_golang "$concurrency"
+        bench_rust   "$concurrency"
+        bench_python "$concurrency"
     done
 }
 
